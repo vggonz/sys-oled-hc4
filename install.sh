1d995c0b10848ae04577d8db85b55620263484fd
Ricardo Pardini <ricardo@pardini.net>
2022-09-06 00:12:45 +0200

    account for Jammy already having packaged python deps

--- install.sh
+++ install.sh
@@ -1,28 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 # Install script for sys-oled
 
 INSTALL_PATH="/usr/local/"
 SYSTEMD_PATH="/etc/systemd/"
-DEPS="python3-dev python3-pip python3-setuptools python3-wheel python3-psutil libfreetype6-dev libjpeg-dev build-essential"
+BUILD_DEPS="python3-dev python3-pip python3-setuptools python3-wheel python3-psutil libfreetype6-dev libjpeg-dev build-essential"
+RUNTIME_DEPS="python3 python3-psutil python3-luma.oled python3-rpi.gpio"
 
 if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root!"
-    exit 1
+	echo "This script must be run as root!"
+	exit 1
 fi
 
 echo "Installing Dependencies"
 apt-get update
-apt-get install -y $DEPS
 
-# luma.oled depends on this thing, but released non-alpha version does not build under gcc10
-echo "Installing GPIO library dependency in pip..."
-pip3 install RPi.GPIO==0.7.1a4
+if [[ "$(lsb_release -cs)" == "jammy" ]]; then
+	echo "Jammy; use Ubuntu-packaged Python deps."
+	apt-get install -y ${RUNTIME_DEPS}
+else
+	echo "Not jammy, installing build deps, then using pip for python stuff. This is gonna take a while."
+	apt-get install -y $BUILD_DEPS
+	# luma.oled depends on this thing, but released non-alpha version does not build under gcc10
+	echo "Installing GPIO library dependency in pip..."
+	pip3 install RPi.GPIO==0.7.1a4
 
-echo "Installing luma.oled library"
-pip3 install --upgrade luma.oled
+	echo "Installing luma.oled library"
+	pip3 install --upgrade luma.oled
+fi
 
 echo "Installing sys-oled files"
-cp -fv etc/sys-oled.conf  /etc
+cp -fv etc/sys-oled.conf /etc
 cp -frv bin "$INSTALL_PATH"
 cp -frv share "$INSTALL_PATH"
 cp -frv system "$SYSTEMD_PATH"
